ccac2129bfdace253870d95ff95f51fad2d3c1ad
Zac <a19860101@hotmail.com>
2018-08-19 14:42:51 +0800

    no-message

--- file.php
+++ file.php
@@ -12,5 +12,31 @@
         <input type="file" name="pic">
         <input type="submit" value="上傳">
     </form>
+    <div class="upload-msg">
+        <?php
+            if(isset($_GET["upload"])){
+                switch($_GET["upload"]){
+                    case 0:
+                        echo "上傳完成";
+                        break;
+                    case 1:
+                        echo "格式錯誤";
+                        break;
+                    case 2:
+                        echo "檔案過大，須小於3M";
+                        break;
+                    case 3:
+                        echo "請選擇檔案";
+                        break;
+                    case 4:
+                        echo "檔案已存在";
+                        break;
+                    case 5:
+                        echo "上傳錯誤";
+                        break;
+                }
+            }
+        ?>
+    </div>
 </body>
 </html>
--- upload.php
+++ upload.php
@@ -1,4 +1,5 @@
 <?php
+    require_once("connDB.php");
     $pic_name = $_FILES["pic"]["name"];
     $pic_size = $_FILES["pic"]["size"];
     $tmpname = $_FILES["pic"]["tmp_name"];
@@ -16,29 +17,57 @@
     // echo $pic_name_file . "<br>";
     // echo $pic_name_dir . "<br>";
 
-    if($pic_name_ext!="jpg" && $pic_name_ext!="png" && $pic_name_ext!="gif"){
-        echo "error";
+    $sql = "INSERT INTO `gallery`(name)VALUES('$pic_name')";
+
+    if($pic_name_ext!="jpg" && $pic_name_ext!="png" && $pic_name_ext!="gif" && $pic_name_ext!="jpeg"){
+        // echo "error";
         $up = 1;
     }
     if($pic_size > $max_size) {
-        echo "檔案過大，須小於3MB";
-        $up = 1;
+        // echo "檔案過大，須小於3MB";
+        $up = 2;
     }
     if($error==4){
-        echo "empty";
-        $up = 1;
+        // echo "empty";
+        $up = 3;
+    }
+    if(file_exists(basename("images/".$pic_name))){
+        $up = 4;
+    }
+    if(isset($up)){
+        switch($up){
+            case 1:
+                header("Location:file.php?upload=1");
+                break;
+            case 2:
+                header("Location:file.php?upload=2");
+                break;
+            case 3:
+                header("Location:file.php?upload=3");
+                break;
+            case 4:
+                header("Location:file.php?upload=4");
+                break;
+        }
+    }else{
+        if(move_uploaded_file($tmpname,$target)){
+            header("Location:file.php?upload=0");
+                mysqli_query($conn,$sql);
+            }else{
+                header("Location:file.php?upload=5");
+            }
     }
 
-    if(isset($up)){
-        echo "上傳失敗";
-    }else{
-        // echo "上傳成功";
-        if(move_uploaded_file($tmpname,$target)){
-            echo "上傳成功"; 
-        }else{
-            echo  "上傳錯誤";
-        }
-    }
+    // if(isset($up)){
+    //     header("Location:file.php?upload=1");
+    // }else{
+    //     if(move_uploaded_file($tmpname,$target)){
+    //         header("Location:file.php?upload=0");
+    //         mysqli_query($conn,$sql);
+    //     }else{
+    //         header("Location:file.php?upload=2");
+    //     }
+    // }
 
     // if($error==0){
     //     if(move_uploaded_file($tmpname,$target)){
